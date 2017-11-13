fd7d31d00ddb4cc06956161ad3ce74ec863d0ffa
seancarnahan <sean.carnahan5@gmail.com>
2017-11-12 20:45:15 -0800

    about an hr and a half of work, getting closer -> having problems with board returning none, prob caused by reverse/sort method

--- connectn.py
+++ connectn.py
@@ -10,7 +10,84 @@ of pieces in a row needed to win are user parameters. If you have never played C
 """
 
 def main():
-    get_game_attributes()
+    is_player_one = True
+    board = []
+
+    rows_columns_win_dependency = get_game_attributes()
+    num_of_rows = rows_columns_win_dependency[0]
+    num_of_columns = rows_columns_win_dependency[1]
+    number_of_pieces_in_a_row_to_win =  rows_columns_win_dependency[2]
+
+    make_empty_board(num_of_rows, num_of_columns, number_of_pieces_in_a_row_to_win)
+
+    board = create_list(num_of_rows, num_of_columns, board)
+    board.reverse()
+
+    play_connectn(num_of_rows, num_of_columns, number_of_pieces_in_a_row_to_win, board, is_player_one)
+
+
+
+
+def play_connectn(num_of_rows, num_of_columns, number_of_pieces_in_a_row_to_win, board, is_player_one):
+
+    while not is_game_over():
+
+        while True:
+            player_move = get_move(num_of_columns)
+            if is_valid_move():
+                break
+            #adding break for right now
+            break
+
+
+        board = update_board(board, player_move, is_player_one)
+        display_board(num_of_rows, num_of_columns, board)
+
+
+        # check winner
+
+
+
+
+
+
+def update_board(board, player_move, is_player_one) -> list:
+    #this method is under the assumption that the move entered is valid and there is room in that column
+    #^ might change and might wanna place that check in this method and handle the error with an exception
+    pass
+
+
+
+
+
+
+def is_valid_move() -> bool:
+
+    # add counter to see if there is even any room on the board
+    pass
+
+
+
+def get_move(num_of_columns) -> int:
+    #this returns a valid player move
+    #not fully done yet because u need to check if the column is full first, prob need more info
+    #use is valid move to help with above comment
+    #there are some problems with this method -> LIKE WHEN you enter the max column it gives an error
+
+    while True:
+        move = input("Enter the column you want to play in: ")
+
+        if not is_int_greater_than_0(move):
+            continue
+        else:
+            move = int(move)
+
+        if 0 <= move or move <= num_of_columns - 1:
+            return move
+
+        else:
+            print("check2")
+            continue
 
 
 
@@ -21,11 +98,131 @@ def main():
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+def is_game_over() -> bool:
+    #should return true if a player won or if its a cats game
+    pass
+
+
+def game_won() -> bool:
+    game_checker = False
+    if row_win():
+        game_checker = True
+    elif col_win():
+        game_checker = True
+    elif diag_win():
+        game_checker = True
+    else:
+        pass
+        # maybe check for cats game here but not sure yet
+
+
+def row_win() -> bool:
+    pass
+
+
+def col_win() -> bool:
+    pass
+
+
+def diag_win() -> bool:
+    diag_win_checker = False
+    if right_diag_win():
+        diag_win_checker = True
+    elif left_diag_win():
+        diag_win_checker = True
+
+    return diag_win_checker
+
+
+def right_diag_win() -> bool:
+    pass
+
+
+def left_diag_win() -> bool:
+    pass
+
+
+def is_tie_game() -> bool:
+    #check to see if there are any astericks in board
+    pass
+
+
+
+
+
+
+
+def create_list(num_of_rows, num_of_columns, board) -> list:
+    # creates a list of astericks, basically the same thing as make empty board but in list form
+
+    for i in range(num_of_columns):
+        board.append([])
+
+    for column in board:
+        for row in range(num_of_rows + 1):
+            column.append("*")
+
+    return board
+
+
+
+
+
+def display_board(num_of_rows, num_of_columns, board) -> None:
+
+    print("  ", end="")
+    for i in range(num_of_columns):
+        print(i, end=" ")
+    print("")
+    current_row = num_of_rows - 1
+    for x in range(num_of_rows):
+        print(current_row, end=" ")
+        print(board)
+        for item in board[x]:
+            print(item, end = " ")
+
+        print("")
+
+
+        current_row -= 1
+
+
+
+
+def make_empty_board(num_of_rows, num_of_columns, number_of_pieces_in_a_row_to_win) -> None:
+    #creates an empty connectn board
+    print("  ", end = "")
+    for i in range(num_of_columns):
+
+        print(i, end=" ")
+    print("")
+    current_row = num_of_rows - 1
+    for i in range(num_of_rows):
+        print(current_row, end= " ")
+
+        for x in range(num_of_columns):
+            print("*", end = " ")
+
+        print("")
+
+        current_row -= 1
 
 
 def get_game_attributes() -> list:
     #returns a list of the game attributes such as num of rows,columns and how many pieces in a row to win
-    rows_columns_win_depedency = []
+    rows_columns_win_dependency = []
     while True:
         number_of_rows = input("Enter the number of rows: ")
 
@@ -56,11 +253,11 @@ def get_game_attributes() -> list:
 
         break
 
-    rows_columns_win_depedency.append(number_of_rows)
-    rows_columns_win_depedency.append(number_of_columns)
-    rows_columns_win_depedency.append(number_of_pieces_in_a_row_to_win)
+    rows_columns_win_dependency.append(number_of_rows)
+    rows_columns_win_dependency.append(number_of_columns)
+    rows_columns_win_dependency.append(number_of_pieces_in_a_row_to_win)
 
-    return rows_columns_win_depedency
+    return rows_columns_win_dependency
 
 
 def is_int_greater_than_0(number) -> bool:
@@ -76,13 +273,6 @@ def is_int_greater_than_0(number) -> bool:
     else:
         return False
 
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     main()
+    #create_list(5,6,[])
